2fbf60eacc7b649f40bf7621693a39d98e5da63c
fonzroevykjunsay <60122963+fonzroevykjunsay@users.noreply.github.com>
2020-12-19 19:43:44 +0800

    Add files via upload

---
+++ clecss/index.php
@@ -0,0 +1,192 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<body>
+<div class="page">
+  <nav class="navbar">
+    <button class="navbar-item">Register</button>
+    <button class="navbar-item">Sign in</button>
+    <div class="anim"></div>
+</nav>
+  <div class="logo">
+      <figure class="face">
+        <span class="eyes">
+          <span class="eye"></span>
+          <span class="eye"></span>
+        </span>
+        <span class="mouth teeth tounge">
+        </span>
+      </figure>
+    <div class="logo-item">Cyanide and Happiness</div>
+    </div>
+    <div class="columns">Meet the characters
+    <div class="column-item">
+    <div class="item1">RED
+    <div class="red angry">
+      <figure class="face">
+        <span class="eyes">
+            <span class="eye"></span>
+            <span class="eye"></span>
+        </span>
+        <span class="mouth">
+        </span>
+      </figure>
+    </div>
+    </div>
+    <div class="item2">BROWN</div>
+    <div class="brown speechless">
+      <figure class="face">
+        <span class="eyes">
+            <span class="eye"></span>
+            <span class="eye"></span>
+        </span>
+        <span class="mouth">
+        </span>
+      </figure>
+    </div>
+    <div class="item3">YELLOW</div>
+    <div class="yellow smile">
+      <figure class="face">
+        <span class="eyes">
+            <span class="eye"></span>
+            <span class="eye"></span>
+        </span>
+        <span class="mouth">
+        </span>
+      </figure>
+    </div>
+    <div class="item4">GREEN</div>
+    <div class="green laugh">
+      <figure class="face">
+        <span class="eyes">
+            <span class="eye"></span>
+            <span class="eye"></span>
+        </span>
+        <span class="mouth tounge">
+        </span>
+      </figure>
+    </div>
+    <div class="item5">BLUE</div>
+    <div class="blue sad">
+      <figure class="face">
+        <span class="eyes">
+            <span class="eye"></span>
+            <span class="eye"></span>
+        </span>
+        <span class="mouth tounge">
+        </span>
+      </figure>
+    </div>
+</div>
+</div>
+<div class="columns">Watch out for our episodes!                                                                                                                                       
+    <div class="column-item1">Bookish Blue</div>
+    <div class="column-item2">Red is something fishy</div>
+    <div class="column-item3">Sad, sad Brown</div>
+    <div class="column-item4">Yellow's cup</div>
+    <div class="column-item5">Green, the handsome butcher</div>
+</div>
+<div>
+<nav class="footer">
+<div class="footer-text"> Did you know by visiting our social media communities you can view your favorite comics, shorts, minis, podcasts, and more? Learn more about it here! </div>
+    <button class="footer-item">Facebook
+    <div class="fb">
+  <div class="facebook fbpos">
+    <a class="fbanchor">F</a></div>
+</div>
+</button>
+    <button class="footer-item">Instagram
+    <a class="insta-footer">
+<div class="insta">
+  <div class="shape"></div>
+</div></a>
+    </button>
+    <button class="footer-item">Google</button>
+    <div class="google-css-logo">
+  <div class="container">
+    <div class="google"></div>
+  </div>
+</div>
+<button class="footer-item">Twitter</button>
+<div class="tw">
+  <div class="twitter twpos"><a class="twanchor">T</a></div>
+</div>
+<div class="animbackground">
+      <figure class="face">
+        <span class="eyes">
+          <span class="eye"></span>
+          <span class="eye"></span>
+        </span>
+        <span class="mouth">
+        </span>
+      </figure>
+</div>
+<div class="animbackground1">
+      <figure class="face">
+        <span class="eyes">
+          <span class="eye"></span>
+          <span class="eye"></span>
+        </span>
+        <span class="mouth teeth tounge">
+        </span>
+      </figure>
+</div>
+<div class="animbackground2">
+      <figure class="face">
+        <span class="eyes">
+          <span class="eye"></span>
+          <span class="eye"></span>
+        </span>
+        <span class="mouth teeth">
+        </span>
+      </figure>
+</div>
+<div class="animbackground3">
+      <figure class="face">
+        <span class="eyes">
+          <span class="eye"></span>
+          <span class="eye"></span>
+        </span>
+        <span class="mouth tounge">
+        </span>
+      </figure>
+</div>
+<div class="animbackground4">
+      <figure class="face">
+        <span class="eyes">
+          <span class="eye"></span>
+          <span class="eye"></span>
+        </span>
+        <span class="mouth ">
+        </span>
+      </figure>
+</div>
+</nav>
+</div>
+
+<ul class="bubbles">
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+</ul>
+</body>
+</head>
+</html>
---
+++ lt2.3/index.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+  
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<body>
+<div class="page">
+  <nav class="navbar">
+    <button class="navbar-item">Register</button>
+    <button class="navbar-item">Sign in</button>
+
+  </nav>
+  <div class="logo">
+      <figure class="face">
+        <span class="eyes">
+          <span class="eye"></span>
+          <span class="eye"></span>
+        </span>
+        <span class="mouth teeth tounge">
+        </span>
+      </figure>
+    <div class="logo-item">Cyanide and Happiness</div>
+    </div>
+  <div class="page-body">
+    <section class="sidebar">
+      <button class="sidebar-item">Home</button>
+      <button class="sidebar-item">Store</button>
+      <button class="sidebar-item">Comics</button>
+      <button class="sidebar-item">Short videos</button>
+      <button class="sidebar-item">News</button>
+      
+      </section>
+
+
+
+    </section>
+       
+    <section class="columns">
+        <article class="column-item">RECENT COMICS AND SHORT VIDEOS</article>
+        <article class="column-item">BEST COMICS OF THE WEEK</article>
+        <article class="column-item">BEST SHORT VIDEOS OF THE WEEK</article>
+    </section>
+  </div>
+</div>
+</div>
+</body>
+</head>
+</html>
